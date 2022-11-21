5d3969c787e9ad407891e4c0c80bbdef3a2ed9dc
adva-mo <advamozes@gmail.com>
2022-11-21 19:32:31 +0200

    Connecting to firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// import { async } from "@firebase/util";
+import { collection, doc, getDocs } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import "./App.css";
+import { db } from "./utils/database-config.js";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const usersCollection = collection(db, "users"); //reference to which collection are we importing
+
+  useEffect(() => {
+    console.log("get");
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    console.log("get");
+    try {
+      const response = await getDocs(usersCollection);
+      console.log(response);
+      setUsers(response.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    } catch (e) {}
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      sharry
+      {/* {users.map(())} */}
+      {console.log(users)}
     </div>
   );
 }
 
 export default App;
+
+// doc.data() function from the firestore library which returns object containing the user info without the id
