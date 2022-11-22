19898ff38855eead68ea0d118f116e7d92ca5468
adva-mo <advamozes@gmail.com>
2022-11-22 19:56:56 +0200

    Navbar added

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <ul>
+      <li>HOME</li>
+      <li>EXPLORE</li>
+      <li>ADD NEW RECIPE</li>
+    </ul>
+  );
+}
+
+export default Navbar;
