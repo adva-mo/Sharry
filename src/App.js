fe047830917437fa81f7c49137b2ce1441dcfd56
adva-mo <advamozes@gmail.com>
2022-11-22 19:56:44 +0200

    Navbar added

--- src/App.js
+++ src/App.js
@@ -4,21 +4,16 @@ import GetUsers from "../src/components/async/GetUsers";
 import Register from "./components/authentication/Register";
 import Logout from "./components/authentication/Logout";
 import Login from "./components/authentication/Login";
+import Navbar from "./components/Navbar/Navbar";
 function App() {
   const [users, setUsers] = useState([]);
 
   return (
     <div>
       sharry
+      <Navbar />
       <GetUsers setUsers={setUsers} />
       {users && <p>fetched all users</p>}
-      {/* <button onClick={() => register()}>register</button> */}
-      {/* {users.length > 0 && <DeleteUser />} */}
-      {/* {users.length > 0 && <UpdateUser />} */}
-      {/* <p>{auth.currentUser.email}</p> */}
-      <Register />
-      <Logout />
-      <Login />
     </div>
   );
 }
