85185956ad25a54b29510fa795a887c47e69e727
adva-mo <advamozes@gmail.com>
2022-11-22 15:44:58 +0200

    Register Component + onAuthStateChanged

--- src/App.js
+++ src/App.js
@@ -1,36 +1,11 @@
 import { useState } from "react";
 import "./App.css";
 import GetUsers from "../src/components/async/GetUsers";
-import AddNewUser from "./components/async/AddNewUser";
-import UpdateUser from "./components/async/UpdateUser";
-import DeleteUser from "./components/async/DeleteUser";
-//*----- auth functions:
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "./utils/database-config";
+import Register from "./components/authentication/Register";
 
 function App() {
   const [users, setUsers] = useState([]);
 
-  const [registerEmail, setRegisterEmail] = useState("");
-  const [registerPassword, setRegisterPassword] = useState("");
-  const [loginEmail, setloginEmail] = useState("");
-  const [loginPassword, setloginPassword] = useState("");
-
-  const register = async () => {
-    console.log("register func");
-    const email = "test@test.com";
-    const password = "test12345";
-    try {
-      //*when creating a new account, the user will be logged in authomatically and the
-      //* user information will be stored in the user variable
-      const user = await createUserWithEmailAndPassword(auth, email, password);
-      console.log(user);
-    } catch (e) {
-      //*firebase does the validations for email
-      // "test@test.com" - passed
-      console.log(e.message);
-    }
-  };
   const login = async () => {};
 
   const logout = async () => {};
@@ -39,10 +14,12 @@ function App() {
     <div>
       sharry
       <GetUsers setUsers={setUsers} />
-      <button onClick={() => register()}>register</button>
+      {users && <p>fetched all users</p>}
+      {/* <button onClick={() => register()}>register</button> */}
       {/* {users.length > 0 && <DeleteUser />} */}
       {/* {users.length > 0 && <UpdateUser />} */}
-      <p>{auth.currentUser.email}</p>
+      {/* <p>{auth.currentUser.email}</p> */}
+      <Register />
     </div>
   );
 }
---
+++ src/components/authentication/Register.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { auth } from "../../utils/database-config";
+
+function Register() {
+  const [user, setUser] = useState({});
+
+  onAuthStateChanged(auth, (currentUser) => {
+    // this functions describe what happens if theres a change in the user login state
+    //it maintains the user connected even if he refresh the web
+    //? check to moce it to a higher component cause itll affect all the app
+    setUser(currentUser);
+  });
+
+  const register = async () => {
+    console.log("register func");
+    // const email = "example2@example2.com";
+    // const password = "test12345";
+    try {
+      //*when creating a new account, the user will be logged in authomatically and the
+      //* user information will be stored in the user variable
+      const user = await createUserWithEmailAndPassword(auth, email, password);
+      console.log(user);
+      const userUID = user.user.uid;
+    } catch (e) {
+      //*firebase does the validations for email
+      // "test@test.com" - passed
+      // !an existing user cant be made twice;
+      console.log(e.message);
+    }
+  };
+  return (
+    <div>
+      <p>registered: {user.email}</p>
+      <button onClick={() => register()}>register</button>
+    </div>
+  );
+}
+
+export default Register;
+//? how to connect a user regustration info to his account?
+// store the uid of the new user and the email, send the user
+// to a next page where he will fill out the rest of he`s profile,
+// and create a new user with the uid and the email adress
