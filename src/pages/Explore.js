779f1424ce12f4adaceccae7d7b31f77d6952d2e
adva-mo <advamozes@gmail.com>
2022-11-23 20:33:05 +0200

    Geting all the recipes

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { getDocs, collection } from "firebase/firestore";
+import { db } from "../utils/database-config.js";
 
 function Explore() {
+  const usersCollection = collection(db, "recipes");
+
+  useEffect(() => {
+    getRecipes();
+  });
+
+  const getRecipes = async () => {
+    try {
+      const response = await getDocs(usersCollection);
+      const state = response.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+      console.log(state);
+    } catch (e) {
+      console.log(e);
+    }
+  };
   return <div>Explore</div>;
 }
 
