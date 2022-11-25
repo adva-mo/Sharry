1ae46c9e72041f6f50058f007a94eae638a8468e
adva-mo <advamozes@gmail.com>
2022-11-25 16:33:34 +0200

    New user data

--- src/components/LoginCard/LoginCard.js
+++ src/components/LoginCard/LoginCard.js
@@ -1,35 +1,78 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "./LoginCard.css";
 
+//todo: user feedbacks
+
 function LoginCard() {
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredEmailTouched, setEnteredEmailTouched] = useState(false);
+
+  const enteredEmailIsValid = enteredEmail.trim() !== "";
+  const emailIsNotValid = !enteredEmailIsValid && enteredEmailTouched;
+
+  const [enteredPassword, setEnteredPassword] = useState("");
+  const [enteredPasswordIsValid, setEnteredPasswordIsValid] = useState(false);
+  //   const [enteredEmailIsValid, setEnteredEmailIsValid] = useState(false);
+  //   const [enteredPasswordTouched, setEnteredPasswordTouched] = useState(false);
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const emailInputChangeHandler = ({ target }) => {
+    setEnteredEmail(target.value);
+  };
+  const passwordInputChangeHandler = ({ target }) => {
+    setEnteredPassword(target.value);
+  };
+
+  const formSubmissionHandler = (e) => {
+    e.preventDefault();
+    setEnteredEmailTouched(true);
+    // setEnteredPasswordTouched(true);
+    if (!enteredEmailIsValid) return;
+    // }
+    setEnteredEmail("");
+    if (enteredPassword.trim() === "" || enteredPassword.length < 6) {
+      setEnteredPasswordIsValid(false);
+      console.log("password not valid");
+      return;
+    }
+    // setEnteredPasswordIsValid(true);
+    emailRef.current.value = "";
+    passwordRef.current.value = "";
+
+    console.log(`enteredEmail: ${enteredEmail}`);
+    console.log(`enteredPassword: ${enteredPassword}`);
+  };
+
   return (
-    // <div className="login-card">
-    <form className="flex-column login-card">
+    <form onSubmit={formSubmissionHandler} className="flex-column login-card">
       <h3>Login and start Sharrying!</h3>
       <label htmlFor="email">email</label>
       <input
+        ref={emailRef}
+        className={"bottom-border"}
         placeholder="enter your email"
-        className="bottom-border"
         type="email"
         name="email"
+        onChange={emailInputChangeHandler}
       />
+      {/* {!enteredEmailIsValid && enteredEmailTouched && (
+        // <p className="error-text">EMAIL MUST NOT BE EMPTY</p>
+      )} */}
       <label htmlFor="password">password</label>
       <input
+        className={"bottom-border"}
+        ref={passwordRef}
         placeholder="enter your password"
-        className="bottom-border"
         type="password"
         name="password"
+        onChange={passwordInputChangeHandler}
       />
-      <label htmlFor="password">repeat your password</label>
-      <input
-        placeholder="repeat your email"
-        className="bottom-border"
-        type="password"
-        name="password"
-      />
+      {/* {!enteredPasswordIsValid && enteredPasswordTouched && (
+        <p className="error-text">PASSWORD MUST NOT BE EMPTY</p>
+      )} */}
       <button className="red-round-btn">Log in</button>
     </form>
-    // </div>
   );
 }
 export default LoginCard;
--- src/components/RecipePreview/RecipePreview.js
+++ src/components/RecipePreview/RecipePreview.js
@@ -17,7 +17,7 @@ function RecipePreview({ color, name, type, img, id }) {
         <div>
           <h3>{name}</h3>
           <p>{type}</p>
-          <a>READ MORE</a>
+          <h4>READ MORE</h4>
         </div>
       </Link>
     </div>
