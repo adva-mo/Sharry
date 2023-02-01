c2991bf082baa343c363c87138426fa4e7f4ff5b
adva-mo <advamozes@gmail.com>
2023-02-01 14:14:03 +0200

    Optimization by using lazy components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
+import { lazy, Suspense, useReducer, useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/homePage/Home";
-import Explore from "./pages/ExplorePage/ExplorePage";
-import LoginPage from "./pages/LoginPage/LoginPage";
-import NewRecipePage from "./pages/newRecipePage/NewRecipePage";
-import RecipeProfile from "./pages/recipeProfilePage/RecipeProfile";
 import { recipesReducers } from "./reducers/recipesReducers.js";
-import { useReducer, useState } from "react";
 import { usersReducers } from "./reducers/usersReducers";
+import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./utils/database-config";
+import currentLoggedUser from "./context/loggedUserContext";
 import "./App.css";
 import "./utils/utils.css";
-import UserProfilePage from "./pages/UserProfilePage";
-import { onAuthStateChanged } from "firebase/auth";
-import currentLoggedUser from "./context/loggedUserContext";
-import NewUserPage from "./pages/newUserPage/NewUserPage";
+import Spinner from "./components/Spinner/Spinner";
+
+const LazyExplore = lazy(() => import("./pages/ExplorePage/ExplorePage"));
+const LazyLoginPage = lazy(() => import("./pages/LoginPage/LoginPage"));
+const LazyUserProfilePage = lazy(() => import("./pages/UserProfilePage"));
+const LazyNewUserPage = lazy(() => import("./pages/newUserPage/NewUserPage"));
+const LazyRecipeProfile = lazy(() =>
+  import("./pages/recipeProfilePage/RecipeProfile")
+);
+const LazyNewRecipePage = lazy(() =>
+  import("./pages/newRecipePage/NewRecipePage")
+);
 
 function App() {
   const [recipes, dispatchRecipes] = useReducer(recipesReducers, null);
@@ -30,55 +36,58 @@ function App() {
       <currentLoggedUser.Provider value={{ ...loggedUser }}>
         <Navbar />
         <>
-          <Routes>
-            {/* <Route path="/" element={<Navigate replace to="/home" />} /> */}
-            <Route path="*" element={<Navigate replace to="/" />} />
-            <Route
-              path="/"
-              element={
-                <Home
-                  recipes={recipes}
-                  dispatchRecipes={dispatchRecipes}
-                  users={users}
-                  dispatchUsers={dispatchUsers}
-                  loggedUser={loggedUser}
-                />
-              }
-            />
-            <Route
-              path="/explore"
-              element={<Explore recipes={recipes} users={users} />}
-            />
-            <Route path="/login" element={<LoginPage />} />
-            <Route
-              path="/users/:id"
-              element={
-                <UserProfilePage
-                  users={users}
-                  recipes={recipes}
-                  dispatchUsers={dispatchUsers}
-                />
-              }
-            />
-            <Route
-              path="/recipe/:id"
-              element={
-                <RecipeProfile
-                  recipes={recipes}
-                  dispatchRecipes={dispatchRecipes}
-                  dispatchUsers={dispatchUsers}
-                />
-              }
-            />
-            <Route
-              path="/new-recipe"
-              element={<NewRecipePage dispatchRecipes={dispatchRecipes} />}
-            />
-            <Route
-              path="/new-user"
-              element={<NewUserPage dispatchUsers={dispatchUsers} />}
-            />
-          </Routes>
+          <Suspense fallback={<Spinner />}>
+            <Routes>
+              <Route path="*" element={<Navigate replace to="/" />} />
+              <Route
+                path="/"
+                element={
+                  <Home
+                    recipes={recipes}
+                    dispatchRecipes={dispatchRecipes}
+                    users={users}
+                    dispatchUsers={dispatchUsers}
+                    loggedUser={loggedUser}
+                  />
+                }
+              />
+              <Route
+                path="/explore"
+                element={<LazyExplore recipes={recipes} users={users} />}
+              />
+              <Route path="/login" element={<LazyLoginPage />} />
+              <Route
+                path="/users/:id"
+                element={
+                  <LazyUserProfilePage
+                    users={users}
+                    recipes={recipes}
+                    dispatchUsers={dispatchUsers}
+                  />
+                }
+              />
+              <Route
+                path="/recipe/:id"
+                element={
+                  <LazyRecipeProfile
+                    recipes={recipes}
+                    dispatchRecipes={dispatchRecipes}
+                    dispatchUsers={dispatchUsers}
+                  />
+                }
+              />
+              <Route
+                path="/new-recipe"
+                element={
+                  <LazyNewRecipePage dispatchRecipes={dispatchRecipes} />
+                }
+              />
+              <Route
+                path="/new-user"
+                element={<LazyNewUserPage dispatchUsers={dispatchUsers} />}
+              />
+            </Routes>
+          </Suspense>
         </>
       </currentLoggedUser.Provider>
     </>
